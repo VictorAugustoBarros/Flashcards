8cb06b9d9ca8fa50649b42969a46af8126ff533b
VictorAugustoBarros <victor.augustobarros@gmail.com>
2021-09-16 21:50:02 -0300

    testando pyqt5

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 """Main."""
 import click
 from app.views.main_view import MainView
+from start import Start_Window
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication
+import sys
 
 
 @click.command()
@@ -10,5 +15,17 @@ def run():
     start_view.mainloop()
 
 
+def run_teste():
+    class ExampleApp(QtWidgets.QMainWindow, Start_Window):
+        def __init__(self, parent=None):
+            super(ExampleApp, self).__init__(parent)
+            self.setupUi(self)
+
+    app = QApplication(sys.argv)
+    form = ExampleApp()
+    form.show()
+    app.exec_()
+
+
 if __name__ == "__main__":
-    run()
+    run_teste()
